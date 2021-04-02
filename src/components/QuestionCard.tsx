a217fda9c4f955942bc46bf8647f4bfb771f32f0
Stefanie Dziadkowiec <stefaniemarie.dz@gmail.com>
2021-04-03 00:56:20 +0200

    init

---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { AnswerObject } from '../App'
+
+type Props = {
+  question: string
+  answers: string[]
+  callBack: (e: React.MouseEvent<HTMLButtonElement>) => void
+  userAnswer: AnswerObject | undefined
+  questionNr: number
+  totalQuestions: number
+}
+
+const QuestionCard: React.FC<Props> = ({
+  question,
+  answers,
+  callBack,
+  userAnswer,
+  questionNr,
+  totalQuestions,
+}) => (
+  <div>
+    <p className="number">
+      Question: {questionNr} / {totalQuestions}
+    </p>
+    <p dangerouslySetInnerHTML={{ __html: question }} />
+    <div>
+      {answers.map((answer) => (
+        <div key={answer}>
+          <button disabled={!!userAnswer} value={answer} onClick={callBack}>
+            <span dangerouslySetInnerHTML={{ __html: answer }} />
+          </button>
+        </div>
+      ))}
+    </div>
+  </div>
+)
+
+export default QuestionCard
